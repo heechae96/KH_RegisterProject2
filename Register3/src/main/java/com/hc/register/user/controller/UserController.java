c0f908d0644313d059c8815fa96d05cc01f816b6
heechae96 <shinheechae@naver.com>
2023-02-23 00:10:23 +0900

    Update

--- Register3/src/main/java/com/hc/register/user/controller/UserController.java
+++ Register3/src/main/java/com/hc/register/user/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.hc.register.user.controller;
 
-import java.io.PrintWriter;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -9,32 +7,30 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.hc.register.Elert;
 import com.hc.register.user.domain.User;
 import com.hc.register.user.service.UserService;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
-
-//	@Autowired
-//	private SubjectService subjectService;
 
 	@Autowired
 	private UserService userService;
 
 	// 로그인
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	@RequestMapping("/login")
 	public String login() {
 		return "user/login";
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(HttpServletRequest request, HttpServletResponse response, Model model) {
-		String id = request.getParameter("id");
-		String pw = request.getParameter("pw");
-		User user = new User(id, pw);
+	public String login(HttpServletRequest request, HttpServletResponse response, 
+			String userId, String userPw, @ModelAttribute User user, Model model) {
 
 		int result = -1;
 		result = userService.checkLogin(user);
@@ -46,23 +42,19 @@ public class UserController {
 		} else {
 			response.setContentType("text/html; charset=UTF-8");
 			try {
-				PrintWriter writer = response.getWriter();
-				String pageURL = "/register/login";
-				writer.println("<script>");
-				writer.println("alert('아이디 또는 비밀번호를 다시 확인해주세요')");
-				writer.println("location.href='" + pageURL + "'");
-				writer.println("</script>");
-				writer.close();
+				Elert elert = new Elert("/user/login", "아이디 또는 비밀번호를 다시 확인해주세요");
+				model.addAttribute("elert", elert);
+				return "common/alert";
 			} catch (Exception e) {
 				model.addAttribute("msg", e.getMessage());
 			}
-			return "redirect:/login";
+			return "redirect:/user/login";
 		}
 
 	}
 	
 	// 로그아웃
-	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	@RequestMapping("/logout")
 	public String logout(HttpServletRequest request) {
 		HttpSession session = request.getSession();
 		if (session != null) {
@@ -70,4 +62,5 @@ public class UserController {
 		}
 		return "common/main";
 	}
+	
 }
