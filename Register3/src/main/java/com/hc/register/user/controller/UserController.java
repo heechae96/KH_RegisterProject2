7e31a925fcbda32ba14e475263aac057bf3d0a62
heechae96 <shinheechae@naver.com>
2023-03-02 02:51:25 +0900

    import 정리

--- Register3/src/main/java/com/hc/register/user/controller/UserController.java
+++ Register3/src/main/java/com/hc/register/user/controller/UserController.java
@@ -13,7 +13,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.hc.register.Alert;
 import com.hc.register.subject.domain.Subject;
